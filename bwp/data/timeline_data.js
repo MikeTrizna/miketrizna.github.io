e1bb5e3d61ab9a91a0cdbc53c578a6d765c16e3d
MikeTrizna <triznam@si.edu>
2014-11-04 09:17:43 -0500

    Changed update dates for MX, NG, ZA

---
+++ bwp/data/timeline_data.js
@@ -0,0 +1,14 @@
+var timeline_data =
+{
+	"last_update": "Monday, 3 November, 2014",
+	"mexico":
+		[
+			{"id": 0, "group": 0, "content": "IBUNAM_LCM_001 (95)", "start": "2014-08-26"},
+		    {"id": 1, "group": 0, "content": "IBUNAM_LCM_002 (95)", "start": "2014-09-03"},
+		    {"id": 2, "group": 0, "content": "IBUNAM_LCM_003 (96)", "start": "2014-09-08"},
+		    {"id": 3, "group": 0, "content": "IBUNAM_LCM_004 (95)", "start": "2014-09-08"},
+		    {"id": 4, "group": 0, "content": "IBUNAM_AJM_001 (96)", "start": "2014-10-14"},
+		    {"id": 5, "group": 0, "content": "IBUNAM_AJM_002 (96)", "start": "2014-10-27"},
+		    {"id": 6, "group": 0, "content": "IBUNAM_AJM_003 (146)", "start": "2014-10-27"}
+		]
+};
